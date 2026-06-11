b3ac94563be46daaba0fc224b07976d550e7dbe7
imashk <sbartek2005@gmail.com>
2026-06-11 19:19:59 +0200

    feat: lab-J

---
+++ lab-J/hello.py
@@ -0,0 +1,4 @@
+name = "Bartosz"
+number = 57794
+
+print(f"Hello {name} ({number}). This environment is using Python version {__import__('sys').version} at location {__import__('sys').executable}.")
